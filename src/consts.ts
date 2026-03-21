3a812e9cdb2a6be3014e72618f7f05a3569e0714
Zgodders207 <zacgodfrey.home@gmail.com>
2026-03-21 15:47:38 +0000

    Update site title and description with personalized content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Zac Godfrey';
+export const SITE_DESCRIPTION = 'Networks & Cyber Security student at Northumbria University. CTF writeups, security projects, and technical blog posts.';
